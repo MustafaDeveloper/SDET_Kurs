85f8c7805ef65d051942113653d65c5bb85e1fcb
developer00 <javadeveloper01@protonmail.com>
2020-09-14 21:22:40 +0200

    12.09.

--- src/Gun01/MainApp.java
+++ src/Gun01/MainApp.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class MainApp {
     public static void main(String[] args) {
-         System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+         System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
          // hangi browserı kullanacağımı ve tarayıcının path ini verdim.
          WebDriver driver = new ChromeDriver(); // çalıştıracağımız driverı kontrol edecek değişkeni tanımladım.
          driver.get("https://www.google.com.tr/"); // siteyi açtım
